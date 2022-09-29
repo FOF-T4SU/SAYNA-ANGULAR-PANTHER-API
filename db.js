165cc4059fb5b570ef15efc428a686c2b4f97b3a
FOF-T4SU <fof.t4su.sayna@gmail.com>
2022-09-29 11:44:00 +0000

    first commit

---
+++ db.js
@@ -0,0 +1,9 @@
+const db = require("./data/db.json");
+const produits = require("./data/produits.json");
+const utilisateursInscrits = require("./data/utilisateursInscrits.json");
+
+module.exports = {
+    db,
+    produits,
+    utilisateursInscrits,
+};
